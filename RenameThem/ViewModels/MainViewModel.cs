70803e76d20c096ebf4cfb7aadc57c6772285c19
kirmir <kirmir@outlook.com>
2015-03-01 19:04:06 +0200

    Implemented view model and logic.

---
+++ RenameThem/ViewModels/MainViewModel.cs
@@ -0,0 +1,90 @@
+﻿using System.ComponentModel;
+using System.IO;
+using System.Runtime.CompilerServices;
+using CS.MVVM.Commands;
+
+namespace RenameThem.ViewModels
+{
+    internal class MainViewModel : INotifyPropertyChanged
+    {
+        private string _renameDirectory;
+        private string _renamePattern;
+        private RelayCommand _renameCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainViewModel(string directory = null)
+        {
+            _renameDirectory = directory;
+            _renamePattern = "{fn}.{ext}";
+            _renameCommand = new RelayCommand(rename, canRename);
+        }
+
+        public string RenameDirectory
+        {
+            get { return _renameDirectory; }
+            set
+            {
+                if (_renameDirectory != value)
+                {
+                    _renameDirectory = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string RenamePattern
+        {
+            get { return _renamePattern; }
+            set
+            {
+                if (_renamePattern != value)
+                {
+                    _renamePattern = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public RelayCommand RenameCommand
+        {
+            get { return _renameCommand; }
+            set
+            {
+                if (_renameCommand != value)
+                {
+                    _renameCommand = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        protected virtual void RaisePropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void rename(object obj)
+        {
+            var files = Directory.GetFiles(RenameDirectory, "*.*", SearchOption.TopDirectoryOnly);
+            foreach (var fullPath in files)
+            {
+                var fileName = Path.GetFileNameWithoutExtension(fullPath);
+                var extension = Path.GetExtension(fullPath);
+                var directory = Path.GetDirectoryName(fullPath);
+
+                var newFileName = RenamePattern.Replace("{fn}", fileName).Replace("{ext}", extension);
+                // ReSharper disable once AssignNullToNotNullAttribute
+                var newFullPath = Path.Combine(directory, newFileName);
+
+                File.Move(fullPath, newFullPath);
+            }
+        }
+
+        private bool canRename(object obj)
+        {
+            return !string.IsNullOrEmpty(_renameDirectory) && !string.IsNullOrEmpty(_renamePattern);
+        }
+    }
+}
--- RenameThem/Views/MainWindow.xaml.cs
+++ RenameThem/Views/MainWindow.xaml.cs
@@ -1,13 +1,31 @@
-﻿namespace RenameThem.Views
+﻿using System;
+using System.Linq;
+using System.Windows.Input;
+using RenameThem.ViewModels;
+
+namespace RenameThem.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    internal partial class MainWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            var args = Environment.GetCommandLineArgs();
+            var directory = args.Count() == 2 ? args[1] : null;
+
+            DataContext = new MainViewModel(directory);
+        }
+
+        private void onKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                Close();
+            }
         }
     }
 }
